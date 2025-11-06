2252306778a2c33999e2be2e902dbe582e12ba4b
jatin vashisth <jatinvashisth91@gmail.com>
2025-11-06 21:23:45 +0530

    new changes

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-uvicorn product.main:app --host 0.0.0.0 --port $PORT
+
+# Ensure dependencies are installed
+pip install --no-cache-dir -r requirements.txt
+# Start the app
+uvicorn product.main:app --host 0.0.0.0 --port 8000
