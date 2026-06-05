e9ca7cde5cea77aa0c4ba36a43ad3f3c26ef6ce4
jatin vashisth <jatinvashisth91@gmail.com>
2026-06-05 21:10:10 +0530

    add ci cd code

--- product/models.py
+++ product/models.py
@@ -8,6 +8,7 @@ class User(Base):
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
+
 class UserData(Base):
     __tablename__ = "user_data"
     id = Column(Integer, primary_key=True, index=True)
